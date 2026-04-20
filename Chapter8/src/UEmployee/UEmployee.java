fe28c0ddceb9d0c710ddbd185dd144b904f62ce3
48004001 <48004001@L815WD220020.edu.cbe.ab.ca>
2026-04-20 13:55:25 -0600

    1:55

--- Chapter8/src/UEmployee/UEmployee.java
+++ Chapter8/src/UEmployee/UEmployee.java
@@ -3,6 +3,18 @@ package UEmployee;
 public class UEmployee 
 {
 
+	private String fName, lName;
+	private double salary;
 	
+	public void Employee(String fn, String ln)
+	{
+		fName = fn;
+		lName = ln;
+	}
+	
+	public String toString()
+	{
+		return(fName + " "+ lName);
+	}
 	
 }
